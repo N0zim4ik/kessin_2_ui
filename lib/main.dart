dd317fed3a6edee2dcb84ac57b6d8a183df2688e
N0zim4ik <muxtorovnozimjon06@gmail.com>
2023-08-15 05:35:09 +0500

    task 5

--- lib/main.dart
+++ lib/main.dart
@@ -5,76 +5,170 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.grey.shade600,
         body: Container(
           child: Column(
             children: [
-              Row(
+              Column(
                 children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 90, top: 60, bottom: 40),
-                    child: Container(
-                      width: 55,
-                      height: 55,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color(0xFFE21C34),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 230, top: 35),
+                        child: Text(
+                          '#E21C34',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25),
-                    child: Text(
-                      '#E21C34',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 20,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 10, top: 60, bottom: 20),
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF364649),
+                          ),
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-              Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color(0xFFE21C34),
-                      Color(0xFF500B28),
                     ],
                   ),
-                  borderRadius: BorderRadius.circular(
-                    10,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 260, right: 25, top: 10),
-                    child: Text(
-                      '#500B28',
-                      style: TextStyle(color: Colors.grey, fontSize: 20),
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 10, top: 60, bottom: 40),
-                    child: Container(
-                      width: 55,
-                      height: 55,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color(0xFF500B28),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [
+                          Color(0xFF364649),
+                          Color(0xFF708F96),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
                       ),
                     ),
                   ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 205, top: 20, bottom: 20),
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF708F96),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          '#E21C34',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color(0xFF708F96),
+                          Color(0xFFAA895F)
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 230, ),
+                        child: Text(
+                          '#AA895F',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 10, top: 20, bottom: 20),
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFAA895F),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [
+                          Color(0xFFAA895F),
+                          Color(0xFFE0d8CC),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 205, top: 20, bottom: 20),
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFEDd8CC),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          '#E0D8CC',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
+
             ],
           ),
         ),
